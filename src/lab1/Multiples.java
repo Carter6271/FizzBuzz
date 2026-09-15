0a9d3bde148124806671ac0b48fdc67bf0560d82
Carter-glitch72 <carterjb627@gmail.com>
2026-09-15 16:53:32 -0400

    changes

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,15 +2,20 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+
+    static int multiples(int n, int a, int b) {
         int total = 0;
-        for (int i = 1; i < 1000; i++) {
-            boolean multipleOfThree = i % 3 == 0;
-            boolean multipleOfFive = i % 5 == 0;
+        for (int i = 1; i < n; i++) {
+            boolean multipleOfThree = i % a == 0;
+            boolean multipleOfFive = i % b == 0;
 
             if (multipleOfFive || multipleOfThree) {
                 total++;
             }
         }
-        System.out.println(total);
+        return total;
     }
 }
