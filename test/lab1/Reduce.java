f6899fbfd9feebe30df6811fef3c223562ec65c8
Carter-glitch72 <carterjb627@gmail.com>
2026-09-15 17:07:25 -0400

    added reduce

---
+++ test/lab1/Reduce.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        int count = reduce(100);
+        System.out.println(count);
+    }
+
+    static int reduce(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n--;
+            }
+        count++;
+        }
+        return count;
+    }
+}
